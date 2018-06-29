5e7f4fdb880c33984327a8b49c7f927354a5b886
Shin-NiL <niltonr@gmail.com>
2018-06-29 10:03:36 -0300

    Add new dependency to Android build
    Add com.android.support:support-v4:27.+ to Android build to properly support FileProvider.

--- share/config.py
+++ share/config.py
@@ -5,6 +5,7 @@ def configure(env):
 	if (env['platform'] == 'android'):
 		env.android_add_java_dir("android")
 		env.android_add_to_manifest("android/AndroidManifestChunk.xml")
+		env.android_add_dependency("compile 'com.android.support:support-v4:27.+'")
 		env.android_add_res_dir("android/res")
 		env.disable_module()
 
