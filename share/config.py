16ce0f4bdc75b3f25269c4b321829a8fea899644
Shin-NiL <niltonr@gmail.com>
2019-04-29 18:51:33 -0300

    Removed conflicting dependency for Godot >= 3.1.0

--- share/config.py
+++ share/config.py
@@ -5,7 +5,8 @@ def configure(env):
 	if (env['platform'] == 'android'):
 		env.android_add_java_dir("android")
 		env.android_add_to_manifest("android/AndroidManifestChunk.xml")
-		env.android_add_dependency("compile 'com.android.support:support-v4:27.+'")
+		#uncomment this line to godot <= 3.0.6
+		#env.android_add_dependency("compile 'com.android.support:support-v4:27.+'")
 		env.android_add_res_dir("android/res")
 		env.disable_module()
 
